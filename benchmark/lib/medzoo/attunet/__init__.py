9614f13fd74ee91869a21d7da75c596056a956c6
Cui Weiwei <34046667+liangjiubujiu@users.noreply.github.com>
2025-06-03 00:01:02 +0800

    Add files via upload

---
+++ benchmark/lib/medzoo/attunet/__init__.py
@@ -0,0 +1,41 @@
+
+from .unet_3D import *
+from .unet_nonlocal_3D import *
+from .unet_grid_attention_3D import *
+
+
+def get_network(name, n_classes, in_channels=3, feature_scale=4, tensor_dim='2D',
+                nonlocal_mode='embedded_gaussian', attention_dsample=(2,2,2),
+                aggregation_mode='concat'):
+    model = _get_model_instance(name, tensor_dim)
+
+    if name in ['unet']:
+        model = model(n_classes=n_classes,
+                      is_batchnorm=True,
+                      in_channels=in_channels,
+                      feature_scale=feature_scale,
+                      is_deconv=False)
+    elif name in ['unet_nonlocal']:
+        model = model(n_classes=n_classes,
+                      is_batchnorm=True,
+                      in_channels=in_channels,
+                      is_deconv=False,
+                      nonlocal_mode=nonlocal_mode,
+                      feature_scale=feature_scale)
+    elif name in ['unet_grid_gating']:
+        model = model(n_classes=n_classes,
+                      is_batchnorm=True,
+                      in_channels=in_channels,
+                      nonlocal_mode=nonlocal_mode,
+                      feature_scale=feature_scale,
+                      attention_dsample=attention_dsample,
+                      is_deconv=False)
+
+    else:
+        raise 'Model {} not available'.format(name)
+
+    return model
+
+
+def _get_model_instance(name, tensor_dim):
+    pass
